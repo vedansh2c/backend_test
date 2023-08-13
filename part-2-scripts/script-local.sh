bcd5d9904524251b59aa46f8ea66bfe4a9418a3d
vedansh <vedansh.dev2001@gmail.com>
2023-08-13 11:53:32 +0530

    cd action completed

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-keypair.pem" ubuntu@ec2-16-170-251-214.eu-north-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
